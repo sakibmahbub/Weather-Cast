0d288a8cb4a9a267638d739d98b7a2a272c1b075
sakibmahbub <arafatsakib48@gmail.com>
2023-03-06 00:28:26 +0600

    displayed weather condition dynamically

--- js/index.js
+++ js/index.js
@@ -1,3 +1,4 @@
+// Api Key only for practicing
 const My_Key = `0d56fb52022a7b44ed8c13c3cda1f498`;
 
 const loadWeatherData = async (city) => {
@@ -7,11 +8,20 @@ const loadWeatherData = async (city) => {
   displayWeatherData(data);
 };
 
+// Displaying data in ui
 const displayWeatherData = (data) => {
-  const temperature = document.getElementById("temperature");
-  temperature.innerText = data.main.temp;
+  setInnerTextById("temperature", data.main.temp);
+
+  setInnerTextById("condition", data.weather[0].main);
 };
 
+// Setting innerText by Id
+const setInnerTextById = (id, text) => {
+  const temperature = document.getElementById(id);
+  temperature.innerText = text;
+};
+
+// Getting city name from the search field
 document.getElementById("btn-search").addEventListener("click", function () {
   const searchfield = document.getElementById("search-field");
   const city = searchfield.value;
